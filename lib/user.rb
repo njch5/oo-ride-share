cde5a3b6c4a114ae9f53b230e2977cd8d0b6f822
CheezItMan <chris@adadevelopersacademy.org>
2018-08-24 11:27:35 -0700

    update initial methods for user

--- lib/user.rb
+++ lib/user.rb
@@ -1,20 +1,31 @@
 module RideShare
-  class Passenger
+  class User
     attr_reader :id, :name, :phone_number, :trips
 
     def initialize(input)
-      if input[:id] == nil || input[:id] <= 0
-        raise ArgumentError.new("ID cannot be blank or less than zero.")
+      if input[:id].nil? || input[:id] <= 0
+        raise ArgumentError, 'ID cannot be blank or less than zero.'
       end
 
       @id = input[:id]
       @name = input[:name]
       @phone_number = input[:phone]
-      @trips = input[:trips] == nil ? [] : input[:trips]
+      @trips = input[:trips].nil? ? [] : input[:trips]
     end
 
-    def get_drivers
-      return @trips.map{ |t| t.driver }
+    def self.load_users(filename = 'support/users.csv')
+      users = []
+
+      CSV.read(filename, headers: true).each do |line|
+        input_data = {}
+        input_data[:id] = line[0].to_i
+        input_data[:name] = line[1]
+        input_data[:phone] = line[2]
+
+        users << User.new(input_data)
+      end
+
+      return users
     end
 
     def add_trip(trip)
