478a8f28b8bc814c9709c236e843dba30c8caf5b
CheezItMan <chris@adadevelopersacademy.org>
2018-08-26 10:28:03 -0700

    update specs

--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -37,8 +37,12 @@ describe "User class" do
 
   describe "trips property" do
     before do
-      @user = RideShare::User.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723", trips: [])
-      trip = RideShare::Trip.new(id: 8, driver: nil, passenger: @user, date: "2016-08-08", rating: 5)
+      @user = RideShare::User.new(id: 9, name: "Merl Glover III",
+                                  phone: "1-602-620-2330 x3723", trips: [])
+      trip = RideShare::Trip.new(id: 8, driver: nil, passenger: @user,
+                                 start_time: Time.parse("2016-08-08"),
+                                 end_time: Time.parse("2016-08-09"),
+                                 rating: 5)
 
       @user.add_trip(trip)
     end
@@ -52,33 +56,6 @@ describe "User class" do
     it "all Trips must have the same passenger's user id" do
       @user.trips.each do |trip|
         expect(trip.passenger.id).must_equal 9
-      end
-    end
-  end
-
-  # Initial Tests for Wave 2
-  describe "get_drivers method" do
-    before do
-      @user = RideShare::User.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723")
-      driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
-      trip = RideShare::Trip.new({id: 8, driver: driver, user: @user, date: "2016-08-08", rating: 5})
-
-      @user.add_trip(trip)
-    end
-
-    it "returns an array" do
-      skip # Unskip after wave 2
-
-      drivers = @user.get_drivers
-      expect(drivers).must_be_kind_of Array
-      expect(drivers.length).must_equal 1
-    end
-
-    it "all items in array are Driver instances" do
-      skip # Unskip after wave 2
-
-      @user.get_drivers.each do |driver|
-        expect(driver).must_be_kind_of RideShare::Driver
       end
     end
   end
