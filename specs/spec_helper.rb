5a0b3a6879bf3ca12e5bc4272229683f551d08ef
CheezItMan <chris@adadevelopersacademy.org>
2018-08-26 12:16:39 -0700

    move require driver

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -9,7 +9,7 @@ require 'awesome_print'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
-#require_relative '../lib/driver'
 require_relative '../lib/user'
 require_relative '../lib/trip'
 require_relative '../lib/trip_dispatcher'
+#require_relative '../lib/driver'
