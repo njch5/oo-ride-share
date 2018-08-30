b4fbd3a0b0048f568f80770fe3f831fc5837f3da
CheezItMan <chris@adadevelopersacademy.org>
2018-08-30 09:23:38 -0700

    add status spec

--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -23,6 +23,10 @@ xdescribe "Driver class" do
         expect{ RideShare::Driver.new(id: 100, name: "George", vin: "33133313331333133extranums")}.must_raise ArgumentError
       end
 
+      it "has a default status of :AVAILABLE" do
+        expect( (RideShare::Driver.new(id: 100, name: "George", vin: "12345678901234567")).status).must_equal :AVAILABLE
+      end
+
       it "sets driven trips to an empty array if not provided" do
         expect(@driver.driven_trips).must_be_kind_of Array
         expect(@driver.driven_trips.length).must_equal 0
