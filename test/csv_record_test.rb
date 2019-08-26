41a2e61932ecede933a474053dec6a4b28cf826f
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-26 13:59:50 -0700

    replaced specs with test in csv file path in setup

--- test/csv_record_test.rb
+++ test/csv_record_test.rb
@@ -1,14 +1,14 @@
-require_relative 'test_helper'
+require_relative "test_helper"
 
 describe RideShare::CsvRecord do
-  describe 'constructor' do
-    it 'takes and saves an id' do
+  describe "constructor" do
+    it "takes and saves an id" do
       id = 7
       record = RideShare::CsvRecord.new(id)
       expect(record.id).must_equal id
     end
 
-    it 'validates the ID' do
+    it "validates the ID" do
       id = -7
       expect {
         RideShare::CsvRecord.new(id)
@@ -16,7 +16,7 @@ describe RideShare::CsvRecord do
     end
   end
 
-  describe 'load_all' do
+  describe "load_all" do
     it "raises an error if neither full_path nor directory is provided" do
       expect {
         RideShare::CsvRecord.load_all
@@ -24,22 +24,22 @@ describe RideShare::CsvRecord do
     end
 
     it "raises an error if invoked directly (without subclassing)" do
-      full_path = 'specs/test_data/testrecords.csv'
+      full_path = "test/test_data/testrecords.csv"
       expect {
         RideShare::CsvRecord.load_all(full_path: full_path)
       }.must_raise NotImplementedError
     end
   end
 
-  describe 'validate_id' do
-    it 'accepts natural numbers' do
+  describe "validate_id" do
+    it "accepts natural numbers" do
       # Should not raise
       [1, 10, 9999].each do |id|
         RideShare::CsvRecord.validate_id(id)
       end
     end
 
-    it 'raises for negative numbers and 0' do
+    it "raises for negative numbers and 0" do
       [0, -1, -10, -9999].each do |id|
         expect {
           RideShare::CsvRecord.validate_id(id)
@@ -47,18 +47,19 @@ describe RideShare::CsvRecord do
       end
     end
 
-    it 'raises for nil' do
+    it "raises for nil" do
       expect {
         RideShare::CsvRecord.validate_id(nil)
       }.must_raise ArgumentError
     end
   end
 
-  describe 'extension' do
+  describe "extension" do
     # It's a class that's designed to be extended.
     # How do you test that? Extend it!
     class TestRecord < RideShare::CsvRecord
       attr_reader :name
+
       def initialize(id:, name:)
         super(id)
         @name = name
@@ -80,34 +81,34 @@ describe RideShare::CsvRecord do
       end
     end
 
-    describe 'load_all' do
+    describe "load_all" do
       let(:record_count) {
-        %x{wc -l 'specs/test_data/testrecords.csv'}.split(' ').first.to_i - 1
+        %x{wc -l 'specs/test_data/testrecords.csv'}.split(" ").first.to_i - 1
       }
-      it 'finds data given just a directory' do
-        directory = 'specs/test_data'
+      it "finds data given just a directory" do
+        directory = "specs/test_data"
         records = TestRecord.load_all(directory: directory)
 
         expect(records.length).must_equal record_count
       end
 
-      it 'finds data given a directory and filename' do
-        directory = 'specs/test_data'
-        file_name = 'custom_filename_test.csv'
+      it "finds data given a directory and filename" do
+        directory = "specs/test_data"
+        file_name = "custom_filename_test.csv"
         records = TestRecord.load_all(directory: directory, file_name: file_name)
 
         expect(records.length).must_equal record_count
       end
 
-      it 'finds data given a full path' do
-        path = 'specs/test_data/custom_filename_test.csv'
+      it "finds data given a full path" do
+        path = "specs/test_data/custom_filename_test.csv"
         records = TestRecord.load_all(full_path: path)
 
         expect(records.length).must_equal record_count
       end
 
-      it 'calls `from_csv` for each record in the file' do
-        TestRecord.load_all(directory: 'specs/test_data')
+      it "calls `from_csv` for each record in the file" do
+        TestRecord.load_all(directory: "specs/test_data")
 
         expect(TestRecord.call_count).must_equal record_count
       end
