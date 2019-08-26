0e2abaa2407eb27db51801b18fe58e258aa83876
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-26 16:46:37 -0700

    Time.parse on start and end time

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,35 +1,47 @@
-require 'csv'
+require "csv"
+require "time"
+require "pry"
 
-require_relative 'csv_record'
+require_relative "csv_record"
 
 module RideShare
   class Trip < CsvRecord
     attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
 
     def initialize(id:,
-      passenger: nil, passenger_id: nil,
-      start_time:, end_time:, cost: nil, rating:)
+                   passenger: nil, passenger_id: nil,
+                   start_time:, end_time:, cost: nil, rating:)
       super(id)
 
       if passenger
         @passenger = passenger
         @passenger_id = passenger.id
-
       elsif passenger_id
         @passenger_id = passenger_id
-
       else
-        raise ArgumentError, 'Passenger or passenger_id is required'
+        raise ArgumentError, "Passenger or passenger_id is required"
       end
 
       @start_time = start_time
       @end_time = end_time
+
+      if @start_time == nil || @end_time == nil
+        raise ArgumentError.new("The time cannot be nil")
+      elsif @start_time == 0 || @start_time == 0
+        raise ArgumentError.new("The time cannot be 0")
+      elsif @start_time > @end_time
+        raise ArgumentError.new("This start time cannot be higher than the end time")
+      end
+
       @cost = cost
       @rating = rating
 
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
+    end
+
+    def valid_time?
     end
 
     def inspect
@@ -46,16 +58,16 @@ module RideShare
     end
 
     private
-    
+
     def self.from_csv(record)
       return self.new(
-        id: record[:id],
-        passenger_id: record[:passenger_id],
-        start_time: record[:start_time],
-        end_time: record[:end_time],
-        cost: record[:cost],
-        rating: record[:rating]
-        )
+               id: record[:id],
+               passenger_id: record[:passenger_id],
+               start_time: Time.parse(record[:start_time]),
+               end_time: Time.parse(record[:end_time]),
+               cost: record[:cost],
+               rating: record[:rating],
+             )
     end
   end
 end
