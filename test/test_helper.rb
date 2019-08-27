feedeb31bd6d9070cc6753422448293d43cc5b37
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-26 21:05:03 -0700

    added require_relative for driver class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,15 @@
-require 'time'
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/skip_dsl'
+require "time"
+require "minitest"
+require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/skip_dsl"
 # Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
-require_relative '../lib/csv_record'
-require_relative '../lib/passenger'
-require_relative '../lib/trip'
-require_relative '../lib/trip_dispatcher'
-# require_relative '../lib/driver'
+require_relative "../lib/csv_record"
+require_relative "../lib/passenger"
+require_relative "../lib/trip"
+require_relative "../lib/trip_dispatcher"
+require_relative "../lib/driver"
